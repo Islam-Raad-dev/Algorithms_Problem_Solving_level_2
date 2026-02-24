b0a4beabdbf514c6b97d48b537982c476fc1b804
Islam Raad <islamraad.dev@gmail.com>
2026-02-24 13:37:02 +0000

    Add Problem 1

--- Algorithms-Problem-Solving-Level-2/Problem-01.cpp
+++ Algorithms-Problem-Solving-Level-2/Problem-01.cpp
@@ -1,9 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int X= 10 , Y=20;
 
-    cout<<X*Y<<endl;
+void PrintTableheader()
+{
+
+    cout << "\n\n\t\t Mulipaiation Table From 1 to 10\n\n";
+    cout << "\t";
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << "\t";
+    }
+    cout << "\n___________________________________________________________________________________\n";
+}
+
+string ColumSperator(int i)
+{
+
+    if (i < 10)
+        return "    |";
+    else
+        return "   |";
+}
+
+void PrintMuliplactionTable()
+{
+
+    PrintTableheader();
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << " " << i << ColumSperator(i) << "\t";
+
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << i * j << "\t";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    PrintMuliplactionTable();
 
     return 0;
 }
