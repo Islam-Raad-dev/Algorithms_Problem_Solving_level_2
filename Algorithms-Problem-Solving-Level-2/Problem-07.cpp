637142fdda9563d397d48b67a71b00d739a25fe7
Islam Raad <islamraad.dev@gmail.com>
2026-03-02 11:43:43 +0000

    Add Problem 7/8

---
+++ Algorithms-Problem-Solving-Level-2/Problem-07.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+
+    int Number = 0;
+
+    do
+    {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+int RevreseNumber(int Number){
+
+    int Remainder = 0 , Number2 = 0;
+
+    while(Number > 0 )
+    {
+
+        Remainder = Number % 10;
+        Number = Number /10;
+        Number2 = Number2 * 10 + Remainder;
+
+    }
+
+
+    return Number2;
+}
+
+int main(){
+
+    cout<<"\nReverse is: "<<RevreseNumber(ReadPositiveNumber("Please Enter Positive Number: "))
+    <<endl;
+
+    return 0;
+}
---
+++ Algorithms-Problem-Solving-Level-2/Problem-08.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+
+    int Number = 0;
+
+    do
+    {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+int CountDigitFrequency(short DigitToCheck, int Number)
+{
+
+    int FreqCount = 0, Remainder = 0;
+
+    while (Number > 0)
+    {
+
+        Remainder = Number % 10;
+        Number = Number / 10;
+
+        if (DigitToCheck == Remainder)
+        {
+            FreqCount++;
+        }
+    }
+
+    return FreqCount;
+}
+
+int main()
+{
+
+    int Number = ReadPositiveNumber("Please Enter A Positive Number: ");
+    short DigitToCheck = ReadPositiveNumber("Please Enter One Digit To Check: ");
+
+    cout << DigitToCheck<<" " << "Frequency is: " << CountDigitFrequency(DigitToCheck, Number) << " Times\n";
+
+    return 0;
+}
