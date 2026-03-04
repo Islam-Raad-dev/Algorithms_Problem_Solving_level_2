6f546bebc3dcd01d093a9cbcea66deae963d050e
Islam Raad <islamraad.dev@gmail.com>
2026-03-04 12:06:29 +0000

    Add Problem 13

---
+++ Algorithms-Problem-Solving-Level-2/Problem-13.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+
+    int Number = 0;
+
+    do
+    {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+int PrintPateern(int Number)
+{
+
+    for (int i = 1; i <= Number; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+
+            cout << i;
+        }
+
+        cout << "\n";
+    }
+}
+
+int main()
+{
+
+    PrintPateern(ReadPositiveNumber("Please Enter Number: "));
+
+    return 0;
+}
