3fe0885020697ca3eb976fed23807df7e182f464
Islam Raad <islamraad.dev@gmail.com>
2026-03-06 14:41:20 +0000

    Add Problem 17

---
+++ Algorithms-Problem-Solving-Level-2/Problem-17.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadPasswordFromUser(string Massege)
+{
+
+    string Password;
+
+    cout << Massege;
+    cin >> Password;
+
+    return Password;
+}
+
+bool GuessPassword(string OringnalPassword)
+{
+
+    cout << "\n";
+    string word = "";
+    int Counter = 0;
+
+    for (int i = 65; i <= 90; i++)
+    {
+        for (int j = 65; j <= 90; j++)
+        {
+            for (int y = 65; y <= 90; y++)
+            {
+
+                Counter++;
+
+                word = word + char(i);
+                word = word + char(j);
+                word = word + char(y);
+
+                cout << "Trail Number [" << Counter << "]" << word << endl;
+
+                if (word == OringnalPassword)
+                {
+                    cout << "\nPassword is " << word << "\n";
+                    cout << "Found After " << Counter << " Trails\n";
+                    return true;
+                }
+                word = "";
+            }
+        }
+    }
+    cout << "\n______________________________________________________\n";
+}
+
+int main()
+{
+    GuessPassword(ReadPasswordFromUser("Please Enter A 3 Letter Password: "));
+
+    return 0;
+}
