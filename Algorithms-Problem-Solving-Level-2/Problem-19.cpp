cb0657307b0504cc20d16bb1e3402f360f2262c0
Islam Raad <islamraad.dev@gmail.com>
2026-03-07 12:53:15 +0000

    Random 3 Number Program

--- Algorithms-Problem-Solving-Level-2/Problem-19.cpp
+++ Algorithms-Problem-Solving-Level-2/Problem-19.cpp
@@ -1,3 +1,25 @@
+// This Program About Print Random Number From - To
+
 #include <iostream>
 #include <string>
+#include <cstdlib>
 using namespace std;
+
+int RandomNumber(int From, int To)
+{
+
+    int randNumb = rand() % (To - From + 1) + From;
+
+    return randNumb;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    cout << RandomNumber(1, 10) << endl;
+    cout << RandomNumber(1, 20) << endl;
+    cout << RandomNumber(1, 30) << endl;
+
+    return 0;
+}
