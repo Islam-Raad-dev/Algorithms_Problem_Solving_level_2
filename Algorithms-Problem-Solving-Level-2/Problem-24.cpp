4f1c6a8b5d0f2a7d34053ccd811fd9562c097cee
Islam Raad <islamraad.dev@gmail.com>
2026-03-10 04:38:09 +0000

    Max Of Random Array

---
+++ Algorithms-Problem-Solving-Level-2/Problem-24.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+
+    int randNumb = rand() % (To - From + 1) + From;
+
+    return randNumb;
+}
+
+void FillArrayWithRandomNumber(int arr[100], int &arrLength)
+{
+
+    cout << "\nEnter Number Of Element: ";
+    cin >> arrLength;
+
+    for (int i = 0; i <= arrLength; i++)
+    {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int arr[100], int &arrLength)
+{
+
+    for (int i = 0; i <= arrLength; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int MaxNumberInArray(int arr[100], int arrLength)
+{
+    int Max = 0;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (arr[i] > Max)
+        {
+            Max = arr[i];
+        }
+    }
+
+    return Max;
+}
+
+int main()
+{
+
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength;
+
+    FillArrayWithRandomNumber(arr, arrLength);
+
+    cout << "\nArray Element: ";
+    PrintArray(arr, arrLength);
+
+    cout << "\nMax Number Is: " << MaxNumberInArray(arr, arrLength) << endl;
+
+    return 0;
+}
