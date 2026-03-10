c026f03b8d3446a5a5631a15d353a3b4bf750c71
Islam Raad <islamraad.dev@gmail.com>
2026-03-10 05:09:03 +0000

    Sum Of Random Array

---
+++ Algorithms-Problem-Solving-Level-2/Problem-26.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+
+    int randNumb = rand() % (To - From + 1) + From;
+
+    return randNumb;
+}
+
+void FillArrayWithRandomNumber(int arr[100], int &arrLength)
+{
+
+    cout << "\nEnter Number Of Element: ";
+    cin >> arrLength;
+
+    for (int i = 0; i <= arrLength; i++)
+    {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int arr[100], int &arrLength)
+{
+
+    for (int i = 0; i <= arrLength; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int SumOFNumberInArray(int arr[100], int arrLength)
+{
+    int Sum = 0;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        Sum += arr[i];
+    }
+
+    return Sum;
+}
+
+int main()
+{
+
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength;
+
+    FillArrayWithRandomNumber(arr, arrLength);
+
+    cout << "\nArray Element: ";
+    PrintArray(arr, arrLength);
+
+    cout << "\nSum Of All Number Is: " << SumOFNumberInArray(arr, arrLength) << endl;
+
+    return 0;
+}
