25befea23f2d25d1f513bacbc770f96b190d6396
Islam Raad <islamraad.dev@gmail.com>
2026-03-12 14:51:01 +0000

    Count Odd Number In Array

---
+++ Algorithms-Problem-Solving-Level-2/Problem-42.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int Random_Number(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void Fill_Array_With_Random_Numbers(int arr[100], int &arrLength)
+{
+    cout << "\nEnter Number of Elements: ";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++)
+        arr[i] = Random_Number(1, 100);
+}
+
+void Print_Array(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+int Count_Odd_Numbers(int arr[100], int arrLength)
+{
+
+    int Counter = 0;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            Counter++;
+        }
+    }
+
+    return Counter;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int arrLength;
+
+    Fill_Array_With_Random_Numbers(arr, arrLength);
+
+    cout << "\nArray Elements:";
+    Print_Array(arr, arrLength);
+
+    cout << "\nOdd Numbers Count Is:";
+    cout << Count_Odd_Numbers(arr, arrLength) << endl;
+
+    return 0;
+}
