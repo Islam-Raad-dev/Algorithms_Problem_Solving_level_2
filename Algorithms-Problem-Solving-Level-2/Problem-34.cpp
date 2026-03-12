87cd56a14c38a86e69183d74506bf613a26d5e17
Islam Raad <islamraad.dev@gmail.com>
2026-03-12 01:33:12 +0000

    Add File

---
+++ Algorithms-Problem-Solving-Level-2/Problem-34.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int Read_Positive_Number(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+int Random_Number(int From, int To)
+{
+
+    int randNumb = rand() % (To - From + 1) + From;
+
+    return randNumb;
+}
+
+void Fill_Array_With_Random_Numbers(int arr[100], int arrLength)
+{
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = Random_Number(1, 100);
+    }
+}
+
+void Print_Array(int arr[100], int arrLength)
+{
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int ReadNumber()
+{
+    int Number;
+
+    cout << "\nPlease Enter A Number To Search For: ";
+    cin >> Number;
+
+    return Number;
+}
+
