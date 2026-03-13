fcbf4998d1b3a331cfc9c1825deef8569179ea01
Islam Raad <islamraad.dev@gmail.com>
2026-03-13 05:04:31 +0000

    Make My ABS Function

--- Algorithms-Problem-Solving-Level-2/Problem-46.cpp
+++ Algorithms-Problem-Solving-Level-2/Problem-46.cpp
@@ -1,61 +1,37 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include<cmath>
+#include <cmath>
 using namespace std;
 
-int Read_Number()
+float Read_Number()
 {
-    int Number;
+    short Number;
 
     cout << "\nPlease Enter A Number: ";
     cin >> Number;
 
     return Number;
 }
-void Cpp_Round(){
 
-    int Round;
-    
-
-}
-
-void Print_Array(int arr[100], int arrLength)
-{
-    for (int i = 0; i < arrLength; i++)
-        cout << arr[i] << " ";
-    cout << "\n";
-}
-
-int Count_Positive_Numbers(int arr[200], int arrLength)
+float My_ABS(int Number)
 {
 
-    int Counter = 0;
+    if (Number > 0)
+        return Number;
 
-    for (int i = 0; i < arrLength; i++)
-    {
-        if (arr[i] >= 0)
-        {
-            Counter++;
-        }
-    }
-
-    return Counter;
+    else
+        return Number * -1;
 }
 
 int main()
 {
 
-    int arr[200];
-    int arrLength;
+    int Number = Read_Number();
 
-    Fill_Array_With_Random_Numbers(arr, arrLength);
+    cout << "\nMy ABS Result:" << My_ABS(Number) << endl;
 
-    cout << "\nMy ABS Result:";
-    Print_Array(arr, arrLength);
-
-    cout << "\nC++ ABS Result:";
-    cout << Count_Positive_Numbers(arr, arrLength) << endl;
+    cout << "\nC++ ABS Result:" << abs(Number) << endl;
 
     return 0;
 }
