582e0c10cfb521c159c4fd8419d2256320d912ee
Islam Raad <islamraad.dev@gmail.com>
2026-03-13 12:29:28 +0000

    Make My S Function

---
+++ Algorithms-Problem-Solving-Level-2/Problem-50.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float Read_Number()
+{
+    short Number;
+
+    cout << "\n Please Enter A Float Number: ";
+    cin >> Number;
+
+    return Number;
+}
+
+float My_Sqrt(float Number)
+{
+    return pow(Number, 0.5);
+}
+
+int main()
+{
+
+    float Number = Read_Number();
+
+    cout << "\nMy Sqrt Result:" << My_Sqrt(Number) << endl;
+
+    cout << "\nC++ Sqrt Result:" << sqrt(Number) << endl;
+
+    return 0;
+}
