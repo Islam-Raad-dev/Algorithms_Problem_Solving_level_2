e38077231c451e7e911715c105c92be0b53e570c
Islam Raad <islamraad.dev@gmail.com>
2026-03-13 12:28:51 +0000

    Make My Round Function

---
+++ Algorithms-Problem-Solving-Level-2/Problem-49.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float Read_Number()
+{
+    short Number;
+
+    cout << "\n Please Enter A Float Number: ";
+    cin >> Number;
+
+    return Number;
+}
+
+
+float Get_Fration_Part(float Number)
+{
+    return Number - int(Number);
+}
+
+float My_Round(float Number)
+{
+
+    int InPart;
+
+    InPart = int(Number);
+
+    float FractionPart = Get_Fration_Part(Number);
+
+    if (abs(FractionPart) >= .5)
+    {
+        if (Number > 0)
+            return ++InPart;
+        else
+            return --InPart;
+    }
+    else
+    {
+        return InPart;
+    }
+}
+int main()
+{
+
+    float Number = Read_Number();
+
+    cout << "\nMy Celi Result:" << My_Ceil(Number) << endl;
+
+    cout << "\nC++ Ceil Result:" << ceil(Number) << endl;
+
+    return 0;
+}
