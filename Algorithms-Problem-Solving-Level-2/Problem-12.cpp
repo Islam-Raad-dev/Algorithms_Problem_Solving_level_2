cd19a55abf15ea04e4f532909ef99028e1e65ce7
Islam Raad <islamraad.dev@gmail.com>
2026-05-30 22:19:06 +0300

    Update Problem-12.cpp

--- Algorithms-Problem-Solving-Level-2/Problem-12.cpp
+++ Algorithms-Problem-Solving-Level-2/Problem-12.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int ReadPositiveNumber(string Message)
 {
 
-    int Number = 0;
+    short Number = 0;
 
     do
     {
         cout << Message;
         cin >> Number;
-    } while (Number <= 0);
+    }   while (Number <= 0);
 
     return Number;
 }
